7bb6f89bdf3463f1c26be86e3244351df2b821ba
omar elgazzar <omelgazzar@gmail.com>
2023-09-20 02:10:17 +0300

    Added profile dropmenu in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,15 +14,49 @@ export default function Navbar() {
       data-bs-theme="dark"
     >
       <div className="container-fluid">
-        <Link to="/" className="navbar-brand">
+        <Link to="/" className="navbar-brand me-auto">
           <img src={logo} alt="logo" style={{ height: 35 }} className="me-2" />
           Appointment Management System
         </Link>
 
         {loggedIn && (
-          <button className="btn btn-secondary" onClick={() => signOut(auth)}>
-            Logout
-          </button>
+          <>
+            <div className="dropdown">
+              <button
+                className="btn d-flex align-items-center text-white text-decoration-none dropdown-toggle"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <img
+                  src="https://github.com/mdo.png"
+                  alt=""
+                  width="32"
+                  height="32"
+                  className="rounded-circle me-2"
+                />
+                <strong>Omar</strong>
+              </button>
+              <ul className="dropdown-menu dropdown-menu-dark text-small shadow">
+                <li>
+                  <button className="dropdown-item">Settings</button>
+                </li>
+                <li>
+                  <button className="dropdown-item">Profile</button>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <button
+                    className="dropdown-item"
+                    onClick={() => signOut(auth)}
+                  >
+                    Sign out
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </>
         )}
       </div>
     </header>
--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ import AuthProvider from "./components/auth/AuthProvider";
 import AppointmentProvider from "./components/appointments/AppointmentProvider";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import rootRoutes from "./routes/root";
+import ErrorProvider from "./components/error/ErrorProvider";
 
-import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import ErrorProvider from "./components/error/ErrorProvider";
+import "./index.css";
 
 const router = createBrowserRouter(rootRoutes);
 
